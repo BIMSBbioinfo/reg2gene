0e1fffc4567af47b381ebeb70a5635c1ba388880
Altuna Akalin <aakalin@gmail.com>
2017-04-15 23:56:31 +0200

    added more function defs for prediction

---
+++ targetPrediction/predUtils.R
@@ -0,0 +1,15 @@
+# this contains utility functions that will be needed for all
+# prediction functions
+
+# function resamples the expression vector
+# returns a list of matrices with shuffled rows for given column
+# B is number of random samples 
+resampleMat<-function(mat,col=1,B=1000){
+  
+}
+
+# function estimates gamma distribution based P-values from the null
+# distribution obtained from resampling
+esimateGammaPval<-function(){
+  
+}
