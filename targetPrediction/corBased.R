bd1458452ad78e5ff58ef901e8c035c619187afd
Altuna Akalin <aakalin@gmail.com>
2017-04-17 20:46:27 +0200

    added cor and dcor based methods, no rigorous testing

--- targetPrediction/corBased.R
+++ targetPrediction/corBased.R
@@ -3,23 +3,81 @@
 
 source("targetPrediction/predUtils")
 
-corMat<-function(mat,method="pearson",col=1){
+# a convenience function that computes the the correlation of a given column
+# with all the other columns
+corMat<-function(y,mat,method="pearson"){
   
-  apply(mat[,-col],2,function(x,y,method){cor(x,y,method=method)},
-        y=mat[,col],method=method)
+  apply(mat,2,function(x,y,method){cor(x,y,method=method)},
+        y=y,method=method)
 }
 
-
-dcorMat<-function(mat,method="pearson",col=1){
+# a convenience function that computes the the distance correlation of a given 
+# column with all the other columns
+dcorMat<-function(y,mat){
   require(energy)
-  apply(mat[,-col],2,function(x,y){dcor(x,y)},
-        y=mat[,col])
+  apply(mat,2,function(x,y){dcor(x,y)},
+        y=y)
 }
 
+# private function that calculate correlation of given
+# column with other columns and assigns significance to
+# correlation coefficents based on resampling
+# @param mat 
+# @param method
+# @param col
+# @param B number of random samples (shuffling) of given column
 corResample<-function(mat,method="pearson",col=1,B=1000){
   
+  # resample response variables Ys
+  Ys=lapply(1:B,function(x) sample(mat[,col],nrow(mat)))
+  
+  # original coeff
+  orig=corMat(y=mat[,col],mat=mat[,-col],method)
+  
+  #coefs
+  coefs=matrix(0.0,ncol=ncol(mat[,-col]),nrow=(B+1))
+  coefs[1,]=orig
+  
+  # calculate coeff for resampled Ys
+  for(i in 1:B){
+    
+    coefs[i+1,] = corMat(Ys[[i]],mat=mat[,-col],method)
+  }
+  
+  # calculate p-vals 
+  pvals=estimateGammaPval(coefs[,-1],orig=coefs[1,],abs=TRUE,add=0)
+  
+  # arrange output format and return
+  return(pvals)
 }
 
-dcorResample<-function(mat,method="pearson",col=1,B=1000){
+# private function that calculats distance correlation of given
+# column with other columns and assigns significance to
+# correlation coefficents based on resampling
+# @param mat 
+# @param col
+# @param B number of random samples (shuffling) of given column
+dcorResample<-function(mat,col=1,B=1000){
   
+  # resample response variables Ys
+  Ys=lapply(1:B,function(x) sample(mat[,col],nrow(mat)))
+  
+  # original coeff
+  orig=dcorMat(y=mat[,col],mat=mat[,-col])
+  
+  #coefs
+  coefs=matrix(0.0,ncol=ncol(mat[,-col]),nrow=(B+1))
+  coefs[1,]=orig
+  
+  # calculate coeff for resampled Ys
+  for(i in 1:B){
+    
+    coefs[i+1,] = dcorMat(Ys[[i]],mat=mat[,-col])
+  }
+  
+  # calculate p-vals 
+  pvals=estimateGammaPval(coefs[,-1],orig=coefs[1,],abs=TRUE,add=0)
+  
+  # arrange output format and return
+  return(pvals)
 }
--- targetPrediction/predUtils.R
+++ targetPrediction/predUtils.R
@@ -1,15 +1,37 @@
 # this contains utility functions that will be needed for all
 # prediction functions
 
-# function resamples the expression vector
-# returns a list of response variable with shuffled rows for given column
-# B is number of random samples 
-resampleMat<-function(mat,col=1,B=1000){
-  lapply(1:B,sample(mat[,col],nrow(mat)))
-}
+
 
 # function estimates gamma distribution based P-values from the null
 # distribution obtained from resampling
-estimateGammaPval<-function(){
+estimateGammaPval<-function(vals,orig,abs=TRUE,add=0){
   
+  
+  if(abs){
+    vals=abs(vals)
+  }
+  if(add){
+    vals=vals+add
+  }
+  
+  if(class(vals) != "matrix"){
+    param <-  fitdistrplus::fitdist(vals,"gamma") # fit gamma
+  
+    pvals=1-pgamma(orig,param$estimate[1],param$estimate[2]) # p-val based on gamma
+    
+    # result vector
+    result=c(coefs=orig,pvals=pvals,p2=sum(vals >= orig)/length(vals))
+  }else{
+    
+    # same as above it is just when the input is a matrix
+    result=mapply(function(x,orig){
+      param <-  fitdistrplus::fitdist(x,"gamma")
+      
+      pvals=1-pgamma(orig,param$estimate[1],param$estimate[2])
+      c(coefs=orig,pvals=pvals,p2=sum(x >= orig)/length(x) )
+    },split(t(vals),1:ncol(vals)),orig)
+    
+  }
 }
+
