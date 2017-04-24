9b1b8c82b937eaacd23c8d95f304049cdde89a30
IngaPa <IngaPa@users.noreply.github.com>
2017-04-24 15:12:30 +0200

    Update produceDataForExamples.R
    Add TSS example.

--- pkg/R/produceDataForExamples.R
+++ pkg/R/produceDataForExamples.R
@@ -29,4 +29,32 @@ setwd("/data/akalin/Projects/AAkalin_reg2gene/reg2gene/")
         
         
 
-TSS <- readRDS("/data/akalin/Projects/AAkalin_Catalog_RI/Data/RPKM_EnhActivity_Pairs_Rdmp/Roadmap/H3K27ac/E")
+ #     GET TSS example   - FAS gene on chr 10  
+  #     Chromosome 10: 90,750,414-90,775,542 forward strand.
+               # TSS a GRanges object that have the TSS location and associated
+               #' gene expression values per cell type or condition as meta data. Each
+               #' row should have a "name" and "name2" columns for unique id or name/symbol
+               #' for the gene which the TSS is associated with. One could be Ensembl id and the
+               #' other could be used for gene symbol.
+               #' Other metadata column names should represent sample names/ids and should
+               #' match the GRanges object provided via regActivity argument.
+        
+               library(stringr)
+               library(GenomicRanges)
+                                    #' 
+                  TSS <- readRDS("/data/akalin/Projects/AAkalin_Catalog_RI/Data/RPKM_EnhActivity_Pairs_Rdmp/Roadmap/H3K27ac/ENSG00000026103_FAS.rds")
+                       
+                  # create GRanges
+                        TSS.location <- colnames(TSS)[1]
+                        TSS.location <- unlist(str_split(TSS.location,"_"))
+                                 TSS.location.gr <-  GRanges(TSS.location[1],IRanges(start=as.integer(TSS.location[2]),end=as.integer(TSS.location[3])))
+                         
+                   # add mcols
+                         name <-  "ENSG00000026103"
+                         name2 <- "FAS"
+                         gene.expression <- t(TSS[,1])
+                         
+                                 mcols(TSS.location.gr) <- cbind(name,name2,gene.expression)
+              
+                            
+                 save(TSS.location.gr,file="pkg/inst/extdata/TSS.RData")
