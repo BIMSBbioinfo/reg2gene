3e922aa38fedf90afaf9056e3d427ff1a4dee647
IngaPa <IngaPa@users.noreply.github.com>
2017-04-24 13:18:30 +0200

    Update produceDataForExamples.R
    Add two bigwig files for chr10 that will be used as an examples

--- pkg/R/produceDataForExamples.R
+++ pkg/R/produceDataForExamples.R
@@ -13,7 +13,19 @@ setwd("/data/akalin/Projects/AAkalin_reg2gene/reg2gene/")
 
 # GET 2 bw files with as an example.
 
+         library(rtracklayer) 
         
+        BW.files.LIVER <- "/data/akalin/Base/RoadmapEpigenomics/Experiment/H3K27ac/bigwig/E066-H3K27ac.fc.signal.bigwig"
+      
+           Chr10.bw = import(BW.files.LIVER, which=seqinfo(BigWigFile(BW.files.LIVER))["chr10"])  
+               export.bw(Chr10.bw,"pkg/inst/extdata/E066-H3K27ac.chr10.fc.signal.bigwig")
+       
+               # E085 Fetal_Intestine_Small
+        BW.files.FIS <- "/data/akalin/Base/RoadmapEpigenomics/Experiment/H3K27ac/bigwig/E085-H3K27ac.fc.signal.bigwig"
+               
+          Chr10.bw = import(BW.files.LIVER, which=seqinfo(BigWigFile(BW.files.LIVER))["chr10"])  
+               export.bw(Chr10.bw,"pkg/inst/extdata/E085-H3K27ac.chr10.fc.signal.bigwig")
+       
         
         
 
