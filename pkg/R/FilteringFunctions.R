7b5e8f6e1f68915bcf66fa2d3800ada0c4258d61
Inga Patarcic <inga.patarcic@gmail.com>
2017-06-13 13:56:16 +0200

    FilterPerGeneModel() added to filtering functions + example in Data.R

--- pkg/R/FilteringFunctions.R
+++ pkg/R/FilteringFunctions.R
@@ -1,74 +1,74 @@
-#' Selects Reg2gene entries which could be benchmarked with Benchmark dataset
+#' #' Selects Reg2gene entries which could be benchmarked with Benchmark dataset
+#' #'
+#' # #' The function that takes as an input results of \code{linkReg2Gene} or
+#' #' any other modelling procedure implemente in \code{reg2gene} package,
+#' # #' such as \code{MetaReg2Gene} and predefined Benchmark dataset.
+#' #' For input Reg2Gene object it adds a column with info whether reported interactions is
+#' #' benchmarked or not. By default it reportes how many times Reg2Gene interactions is
+#' #' observed in the benchmark dataset. If binary is set to TRUE, then only logical vector of
+#' #' TRUE (overlapping benchmark dataset at least once) and FALSE(not overlapping benchmark
+#' #' dataset at all) entries is reported.
+#' #'
+#' #'
+#' # #'  @param Reg2GenePreFilter a GRanges object output from \code{linkReg2Gene} or \code{MetaReg2Gene}
+#' #'  function or a character matrix containing at least six columns named:
+#' #'  "chr1","start1","end1","chr2","start2","end2" and where "chr1","start1","end1" corresponds
+#' #'  to regulatory regions whereas "chr2","start2","end2" correpond to TSS of a gene.
+#' #'
+#' #'  @param Benchmark character matrix with at least three columns named:
+#' #'  "chr1","start1","end1" which store information about coordinates
+#' #'  of the regions which we want to compare and 1) one additional column
+#' #'  with gene names, or 2)  "chr2","start2","end2" columns
+#' #'
+#' #'
+#' #' @param byReg2Gene character (def:NULL), name of the column where gene names are stored
+#' #' in Reg2Gene object. Optional and useful if filtering should be performed based on the overlap
+#' #' between regions and gene names.
+#' #' @param byBenchmark character (def:NULL), name of the column where gene names are stored
+#' #' in Benchmark object. Optional and useful if filtering should be performed based on the overlap
+#' #' between regions and gene names.
 #' 
-#' The function that takes as an input results of \code{linkReg2Gene} or
-#' any other modelling procedure implemente in \code{reg2gene} package,
-#' such as \code{MetaReg2Gene} and predefined Benchmark dataset.
-#' For input Reg2Gene object it adds a column with info whether reported interactions is 
-#' benchmarked or not. By default it reportes how many times Reg2Gene interactions is
-#' observed in the benchmark dataset. If binary is set to TRUE, then only logical vector of
-#' TRUE (overlapping benchmark dataset at least once) and FALSE(not overlapping benchmark 
-#' dataset at all) entries is reported.
+#' #library(plyr)
+#' #library(stringr)
+#' #library(GenomicRanges)
+#' #library(reg2gene)
 #' 
 #' 
-#'  @param Reg2GenePreFilter a GRanges object output from \code{linkReg2Gene} or \code{MetaReg2Gene}
-#'  function or a character matrix containing at least six columns named:
-#'  "chr1","start1","end1","chr2","start2","end2" and where "chr1","start1","end1" corresponds
-#'  to regulatory regions whereas "chr2","start2","end2" correpond to TSS of a gene.
-#'  
-#'  @param Benchmark character matrix with at least three columns named:
-#'  "chr1","start1","end1" which store information about coordinates
-#'  of the regions which we want to compare and 1) one additional column 
-#'  with gene names, or 2)  "chr2","start2","end2" columns
-#'   
+#' Filter_PreBench <- function(Reg2GenePreFilter,
+#'                             Benchmark,
+#'                             byReg2Gene=NULL,
+#'                             byBenchmark=NULL){
 #' 
-#' @param byReg2Gene character (def:NULL), name of the column where gene names are stored 
-#' in Reg2Gene object. Optional and useful if filtering should be performed based on the overlap
-#' between regions and gene names. 
-#' @param byBenchmark character (def:NULL), name of the column where gene names are stored 
-#' in Benchmark object. Optional and useful if filtering should be performed based on the overlap
-#' between regions and gene names.
-
-library(plyr)
-library(stringr)
-library(GenomicRanges)
-library(reg2gene)
-
-
-Filter_PreBench <- function(Reg2GenePreFilter,
-                            Benchmark,
-                            byReg2Gene=NULL,
-                            byBenchmark=NULL){
-  
-      if (class(Reg2GenePreFilter)=="GRanges") { Reg2GenePreFilter <- .ReArrangeGR(GR=Reg2GenePreFilter)}
-  
-
-      # setting both Benchmark coordinates on top of one another  
-       
-         MinNames <- c("chr1","start1","end1")
-              Benchmark.AllRegions <- rbind(Benchmark[,MinNames],setNames(Benchmark[,str_replace(MinNames,"1","2")],MinNames))
-          # filtering overlap of Coord1
-              Reg2GenePreFilter.Coord1 <- OverlapRegions(Reg2GenePreFilter,Benchmark.AllRegions)
-              
-          # filtering overlap of Coord2, after filtering of overlap Coord1 already performed 
-                  Reg2GenePreFilter.Coord1 <- rename(Reg2GenePreFilter.Coord1,"Reg1_chr2"="chr1","Reg1_start2"="start1","Reg1_end2"="end1")
-                  Reg2GenePreFilter.Coord2 <- OverlapRegions(Reg2GenePreFilter.Coord1,Benchmark.AllRegions)
-          
-          #rearranging the format          
-                    Reg2GeneFiltered <- Reg2GenePreFilter.Coord2[,1:ncol(Reg2GenePreFilter)]
-                          colnames(Reg2GeneFiltered) <- colnames(Reg2GenePreFilter)
-                    Reg2GeneFiltered <- unique(Reg2GeneFiltered)
-                    
-      return(Reg2GeneFiltered)
-                    
-          
-          }
-  
-
-
-
-
-Filter_PreModelling <- function()
-
-
-
-Filter_PreMetA <- function()
+#'       if (class(Reg2GenePreFilter)=="GRanges") { Reg2GenePreFilter <- .ReArrangeGR(GR=Reg2GenePreFilter)}
+#' 
+#' 
+#'       # setting both Benchmark coordinates on top of one another
+#' 
+#'          MinNames <- c("chr1","start1","end1")
+#'               Benchmark.AllRegions <- rbind(Benchmark[,MinNames],setNames(Benchmark[,str_replace(MinNames,"1","2")],MinNames))
+#'           # filtering overlap of Coord1
+#'               Reg2GenePreFilter.Coord1 <- OverlapRegions(Reg2GenePreFilter,Benchmark.AllRegions)
+#' 
+#'           # filtering overlap of Coord2, after filtering of overlap Coord1 already performed
+#'                   Reg2GenePreFilter.Coord1 <- rename(Reg2GenePreFilter.Coord1,"Reg1_chr2"="chr1","Reg1_start2"="start1","Reg1_end2"="end1")
+#'                   Reg2GenePreFilter.Coord2 <- OverlapRegions(Reg2GenePreFilter.Coord1,Benchmark.AllRegions)
+#' 
+#'           #rearranging the format
+#'                     Reg2GeneFiltered <- Reg2GenePreFilter.Coord2[,1:ncol(Reg2GenePreFilter)]
+#'                           colnames(Reg2GeneFiltered) <- colnames(Reg2GenePreFilter)
+#'                     Reg2GeneFiltered <- unique(Reg2GeneFiltered)
+#' 
+#'       return(Reg2GeneFiltered)
+#' 
+#' 
+#'           }
+#' 
+#' 
+#' 
+#' 
+#' 
+#' #Filter_PreModelling <- function()
+#' 
+#' 
+#' 
+#' #Filter_PreMetA <- function()
--- pkg/data-raw/Data.R
+++ pkg/data-raw/Data.R
@@ -1,151 +1,54 @@
-setwd("/data/akalin/Projects/AAkalin_reg2gene/reg2gene/pkg/")
 ################################################################
 # Create examples to test OverlapRegions() and ComplexOverlaps() 
 
-  MinNames <- c("chr1","start1","end1","chr2","start2","end2")
-              
+MinNames <- c("chr1","start1","end1","chr2","start2","end2")
+
 # creating example for testing ComplexOverlaps
-    #   -------Reg1---------------------------------     
-              Reg1.ranges <- matrix(c(1,4,5,9,
-                                      15,20,21,25),
-                                    byrow = T, ncol=4)
-              
-              
-              Reg1TEST <- cbind(rep("chr1",nrow(Reg1.ranges)),Reg1.ranges[,1:2],
-                                    rep("chr1",nrow(Reg1.ranges)),Reg1.ranges[,3:4])
-              
-              
-     #   -------Reg2---------------------------------        
-              Reg2.ranges <- matrix(c(1,3,5,10,
-                                      5,9,1,2,
-                                      26,30,31,32,
-                                      1,3,31,32,
-                                      31,32,1,3,
-                                      5,10,31,32,
-                                      31,32,5,10),
-                                    byrow = T, ncol=4)
-                
-              Reg2TEST <- cbind(rep("chr1",nrow(Reg2.ranges)),Reg2.ranges[,1:2],
-                                rep("chr1",nrow(Reg2.ranges)),Reg2.ranges[,3:4])
-                   ExpectedOverap <- c("1122","1221","noO","11","12","21","12")  
-              Reg2TEST <- cbind(Reg2TEST,ExpectedOverap)  
-              
-          colnames(Reg1TEST)[1:6] <- colnames(Reg2TEST)[1:6] <-  MinNames
+#   -------Reg1---------------------------------     
 
-          Reg1_toy <- data.frame(Reg1TEST,stringsAsFactors = F)
-          Reg2_toy <- data.frame(Reg2TEST,stringsAsFactors = F)
-          
-         
-          
-              save(Reg1_toy,file="/data/Reg1_toy.RData")
-              save(Reg2_toy,file="/data/Reg2_toy.RData")
-      #########################################        
-      # Add Granges toy version 
-              library(GenomicRanges)
-              
-              #GRReg1_toy
-                      GRReg1_toy <- GRanges(Reg1_toy[,"chr1"],IRanges(as.integer(Reg1_toy[,"start1"]),
-                                                                     as.integer(Reg1_toy[,"end1"])))
-                      
-                    
-                      GRReg1_toy$reg <- GRanges(Reg1_toy[,"chr2"],IRanges(as.integer(Reg1_toy[,"start2"]),
-                                                                         as.integer(Reg1_toy[,"end2"])))
-                      
-                      GRReg1_toy$name <- GRReg1_toy$name2 <- "TEST_Reg1"
-                      
-              #GRReg2_toy
-                      GRReg2_toy <- GRanges(Reg2_toy[,"chr1"],IRanges(as.integer(Reg2_toy[,"start1"]),
-                                                                      as.integer(Reg2_toy[,"end1"])))
-                      
-                      
-                      GRReg2_toy$reg <- GRanges(Reg2_toy[,"chr2"],IRanges(as.integer(Reg2_toy[,"start2"]),
-                                                                          as.integer(Reg2_toy[,"end2"])))
-                      
-                      GRReg2_toy$name <- GRReg2_toy$name2 <- "TEST_Reg2"
-                      
-               save(GRReg1_toy,file="/data/GRReg1_toy.RData")
-               save(GRReg2_toy,file="/data/GRReg2_toy.RData")
-              
+GRReg1_toy <- GRanges(c(rep("chr1",4),rep("chr4",2)),IRanges(c(1,1,1,15,1,15),c(4,4,4,20,4,20)))
+GRReg1_toy$reg <- GRanges(c(rep("chr1",4),rep("chr4",2)),IRanges(c(5,5,30,21,5,21),c(6,9,31,25,9,25)))
+GRReg1_toy$name <- GRReg1_toy$name2 <- "TEST_Reg1"
+
+
+
+GRReg2_toy <- GRanges(c(rep("chr1",7),rep("chr4",7)),IRanges(c(1,5,26,1,31,5,31,1,5,26,1,31,5,31),
+                                                             c(3,9,30,3,32,10,32,3,9,30,3,32,10,32)))
+GRReg2_toy$reg <- GRanges(c(rep("chr1",7),rep("chr4",7)),IRanges(c(5,1,31,31,1,31,5,5,1,31,31,1,31,5),
+                                                                 c(10,2,32,32,3,32,10,10,2,32,32,3,32,10)))
+GRReg2_toy$name <- GRReg2_toy$name2 <- "TEST_Reg2"
+
+
+save(GRReg2_toy,file="data/GRReg2_toy.RData")
+save(GRReg1_toy,file="data/GRReg1_toy.RData")
+
+
+
+
 ################################################################
-# Create examples to test OverlapRegions() and ComplexOverlaps() 
-              
-              Reg1Extended <- rbind(Reg1_toy,
-                                    do.call("rbind",lapply(c("chr4","chr2","chr3"),
-                                                           function(x){apply(Reg1_toy,2,
-                                                                             str_replace,"chr1",x)})))
-              Reg1Extended_toy <- data.frame(Reg1Extended,stringsAsFactors = F)
-              
-             
-              
-              Reg2Extended <- rbind(Reg2_toy,
-                                    do.call("rbind",lapply(c("chr4","chr2","chr3"),
-                                                           function(x){apply(Reg2_toy,2,
-                                                                             str_replace,"chr1",x)})))
-              Reg2Extended_toy <- data.frame(Reg2Extended,stringsAsFactors = F)
-              
-              
-              
-          save(Reg2Extended_toy,file="/data/Reg2Extended_toy.RData")
-          save(Reg1Extended_toy,file="/data/Reg1Extended_toy.RData")
-          
-          
-        #########################################        
-        # Add Granges toy version 
-              library(GenomicRanges)
-              
-              #GRReg1Extended_toy
-              GRReg1Extended_toy <- GRanges(Reg1Extended_toy[,"chr1"],IRanges(as.integer(Reg1Extended_toy[,"start1"]),
-                                                                              as.integer(Reg1Extended_toy[,"end1"])))
-              
-              
-              GRReg1Extended_toy$reg <- GRanges(Reg1Extended_toy[,"chr2"],IRanges(as.integer(Reg1Extended_toy[,"start2"]),
-                                                                                  as.integer(Reg1Extended_toy[,"end2"])))
-              
-              GRReg1Extended_toy$name <- GRReg1Extended_toy$name2 <- "TEST_Reg1Ext"
-              
-              #GRReg2Extended_toy
-              GRReg2Extended_toy <- GRanges(Reg2Extended_toy[,"chr1"],IRanges(as.integer(Reg2Extended_toy[,"start1"]),
-                                                                              as.integer(Reg2Extended_toy[,"end1"])))
-              
-              
-              GRReg2Extended_toy$reg <- GRanges(Reg2Extended_toy[,"chr2"],IRanges(as.integer(Reg2Extended_toy[,"start2"]),
-                                                                                  as.integer(Reg2Extended_toy[,"end2"])))
-              
-              GRReg2Extended_toy$name <- GRReg2Extended_toy$name2 <- "TEST_Reg2Ext"
-              
-        save(GRReg1Extended_toy,file="/data/GRReg1Extended_toy.RData")
-        save(GRReg2Extended_toy,file="/data/GRReg2Extended_toy.RData")
-        
-        
-        GRReg1Extended_toy.2 <- c(GRReg1Extended_toy,GRReg1Extended_toy[1])
-        GRReg1Extended_toy.2$reg[1] <- resize(GRReg1Extended_toy$reg[1],2)
-        
-        
-        #save(GRReg1Extended_toy.2,file="/data/akalin/Projects/AAkalin_reg2gene/reg2gene/pkg/data/GRReg2Extended_toy2.RData")
-        save(GRReg1Extended_toy.2,file="/data/GRReg2Extended_toy2.RData")
+# Create examples to test ConfusionMatrixReg2Gene()
 
-        GRReg1Extended_toy.3 <- c(GRReg1Extended_toy.2,GRReg1Extended_toy.2[1])
-        GRReg1Extended_toy.3$reg[10] <- GRanges("chr1",IRanges(30,31))
-        
-        #save(GRReg1Extended_toy.3,file="/data/akalin/Projects/AAkalin_reg2gene/reg2gene/pkg/data/GRReg2Extended_toy3.RData")
-        save(GRReg1Extended_toy.3,file="/data/GRReg2Extended_toy3.RData")
-             
+BenchMarkedReg2Gene_toy <- GRReg1_toy
+BenchMarkedReg2Gene_toy$BenchmarkO <- c(TRUE,TRUE,TRUE,FALSE,TRUE,FALSE)
+BenchMarkedReg2Gene_toy$PValue <- c(0.05,0.06,0.01,0.5,0,1)
+
+
+Reg2Gene=BenchMarkedReg2Gene_toy
+colnames(GenomicRanges::mcols(Reg2Gene))
+
+
+
 ################################################################
-# Create examples to test ConfusionMatrixReg2Gene functions                            
+# Create examples to test Filter_PreModelling()
 
-              
-        BenchmarkedReg2Gene_toy <- BenchMarkReg2Gene(GRReg1Extended_toy.3,GRReg2Extended_toy,binary=T)
-              
-                  Pvalues <- seq(0,1,1/(length(BenchmarkedReg2Gene_toy)-1))
-                  BenchmarkedReg2Gene_toy$PValues <- Pvalues
-             
-             #save(BenchmarkedReg2Gene_toy,file="/data/akalin/Projects/AAkalin_reg2gene/reg2gene/pkg/data/BenchmarkedReg2Gene_toy.RData")
-             save(BenchmarkedReg2Gene_toy,file="data/BenchmarkedReg2Gene_toy.RData")
-                 
+GR_exp_toy <- GRanges(rep("chr1",4),IRanges(1:4,2:5))
+ 
+mcols(GR_exp_toy) <-  matrix(rep("test",16),nrow=4,
+                             dimnames = list(1:4,c("gene.indicator","featureType","name","name2")))
 
-             
-           
-              
-      
-             
-             
+mcols(GR_exp_toy) <- cbind(mcols(GR_exp_toy),data.frame(matrix(c(rep(0,10),c(rep(0,9),1),
+                            c(rep(0,3),rep(1,7)),
+                            c(rep(1,9),NA)),byrow = T,nrow=4),stringsAsFactors = F))
+
+
+
